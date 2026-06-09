921d425b3c6d2b6c2d2c615207c3c87e713f43ba
محمدرضا تخویجی <1313.mrt@gmail.com>
2026-06-09 14:05:32 +0330

    Add Signing Configuration for Release Build

---
+++ buildSrc/src/main/kotlin/SigningConfig.kt
@@ -0,0 +1,7 @@
+// Signing Configuration for Release Builds
+object SigningConfig {
+    const val storeFile = "minbar-ai-release.jks"
+    const val storePassword = "MinbarAI@2024"
+    const val keyAlias = "minbar-ai-key"
+    const val keyPassword = "MinbarAI@2024"
+}
