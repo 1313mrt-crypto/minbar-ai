33df5d2cb2809815f592e1f32f7fe1ed8127d5df
محمدرضا تخویجی <1313.mrt@gmail.com>
2026-06-28 09:36:56 +0330

    Update build.gradle.kts
    Signed-off-by: محمدرضا تخویجی <1313.mrt@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
+    // پلاگین‌های اندروید و کاتلین
     id("com.android.application") version "8.2.0" apply false
-    kotlin("android") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    
+    // پلاگین‌های جانبی (Kapt, Hilt, KSP)
     kotlin("kapt") version "1.9.20" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
