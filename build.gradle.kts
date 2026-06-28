f4827bc7fe14aa1f1e8295699da46a81f35fb729
محمدرضا تخویجی <1313.mrt@gmail.com>
2026-06-29 01:14:07 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "9.2.0" apply false
     id("com.android.library") version "9.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false // از ورژن 2.0.21 استفاده کنید
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
+   id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
 }
