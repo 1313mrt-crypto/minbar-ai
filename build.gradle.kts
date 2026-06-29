1b149a19e290c0d8d56fb5662d44401e788f03d2
محمدرضا تخویجی <1313.mrt@gmail.com>
2026-06-29 06:01:18 +0330

    Update build.gradle.kts
    Signed-off-by: محمدرضا تخویجی <1313.mrt@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "9.2.0" apply false
     id("com.android.library") version "9.2.0" apply false
-   id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.dagger.hilt.android") version "2.59.2" apply false
     id("com.google.devtools.ksp") version "2.3.9" apply false
 }
